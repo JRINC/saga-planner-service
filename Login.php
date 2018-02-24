24bca50df8e1eec1ce5cf7ec76b42c7f9f4c0859
Luis Hernández <junior9482@gmail.com>
2018-02-24 00:20:48 +0000

    first commit

---
+++ Login.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$username = $_GET['fname'];
+$password = $_GET['fpass'];
+$con=mysqli_connect("localhost","jrinc","","c9");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+// $qz = "SELECT id, email FROM members where username=".$username." and password=".$password."" ; 
+$qz = "SELECT id, email FROM members where email=".$username." and password=".$password."" ; 
+//printf($qz);
+$qz = str_replace("\'","",$qz); 
+$result = mysqli_query($con,$qz);
+while($row = mysqli_fetch_array($result))
+  {
+      echo $row['id'];
+    //   echo $row['email'];
+  }
+mysqli_close($con);
+?>
---
+++ Register.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$useremail = $_GET['femail'];
+$password = $_GET['fpass'];
+$con=mysqli_connect("localhost","jrinc","","c9");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+// $qz = "SELECT id, email FROM members where username=".$username." and password=".$password."" ; 
+$qz = "INSERT INTO members (username, email, password) VALUES (".$useremail.", ".$useremail.", ".$password.")" ; 
+// printf($qz);
+$qz = str_replace("\'","",$qz); 
+$result = mysqli_query($con,$qz);
+echo $result;
+mysqli_close($con);
+?>
